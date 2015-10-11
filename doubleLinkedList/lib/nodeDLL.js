79879b4d2a0c49a899ba4697b68eddac7e7ee4fb
Brayzen <brian.ray.biz@gmail.com>
2015-10-10 20:39:31 -0700

    DoubleLinkedList Working great...tests are a B

--- doubleLinkedList/lib/nodeDLL.js
+++ doubleLinkedList/lib/nodeDLL.js
@@ -1,7 +1,7 @@
 var Node = function(val, prv, nxt) {
   this.val = val;
-  this.prv = prv || null;
-  this.nxt = nxt || null;
+  this.prv = prv != null ? prv : null;
+  this.nxt = nxt != null ? nxt : null;
 }
 
 module.exports = Node;
